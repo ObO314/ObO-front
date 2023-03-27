05e48d3a5e30808c71efecf4520ff635004c2a6c
Gyuri092 <ggr0927@gmail.com>
2023-03-27 15:26:32 +0900

    init: 프로젝트 초기 세팅

---
+++ client/src/router.tsx
@@ -0,0 +1,29 @@
+// import ParticipantResult from '@pages/ParticipantResult';
+// import { ShareLink } from '@pages/ShareLink';
+// import React from 'react';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import ResultPage from '@pages/ResultPage';
+// import ChartResultPage from '@pages/ChartResultPage';
+// import CompareResultPage from '@pages/CompareResultPage';
+// import MainPage from './pages/MainPage';
+// import QuizMainPage from '@pages/QuizMainPage';
+// import QuizForm from '@pages/QuizForm';
+
+// const Router = () => {
+//     return (
+//         <BrowserRouter>
+//             <Routes>
+//                 <Route path="/" element={<MainPage />} />
+//                 <Route path="/:userKey" element={<QuizMainPage />} />
+//                 <Route path="/:userKey/share" element={<ShareLink />} />
+//                 <Route path="/:userKey/result" element={<ResultPage />} />
+//                 <Route path="/:userKey/result/chart" element={<ChartResultPage />} />
+//                 <Route path="/:userKey/result/compare" element={<CompareResultPage />} />
+//                 <Route path="/:userKey/participant/quiz" element={<QuizForm />} />
+//                 <Route path="/:userKey/participant/result" element={<ParticipantResult />} />
+//             </Routes>
+//         </BrowserRouter>
+//     );
+// };
+
+// export default Router;
