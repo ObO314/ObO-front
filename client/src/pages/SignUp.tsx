99fb3ed93f327f8d55815ae696fdbae8cc0ced0c
Gyuri092 <ggr0927@gmail.com>
2023-04-03 16:17:50 +0900

    style: 반응형 일부 반영

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -50,14 +50,14 @@ export default function SignUp() {
     );
 }
 
-const StyledContainer = styled.div`
+export const StyledContainer = styled.div`
     width: 100%;
     height: 100%;
     display: flex;
     margin-top: 3rem;
 `;
 
-const StyledBox = styled.div`
+export const StyledBox = styled.div`
     width: 50%;
     height: 60%;
     display: flex;
@@ -65,28 +65,28 @@ const StyledBox = styled.div`
     align-items: center;
 `;
 
-const StyledSignUpBox = styled.div`
+export const StyledSignUpBox = styled.div`
     width: 90%;
-    height: 90%;
+    height: 60vh;
     border: 1px solid var(--gray-color);
     border-radius: 10px;
     background-color: #ffffff;
     padding: 1rem;
 `;
 
-const StyledSignUpContainer = styled.div`
+export const StyledSignUpContainer = styled.div`
     width: 100%;
     height: 40%;
 `;
 
-const StyledSignUpTileBox = styled.div`
+export const StyledSignUpTileBox = styled.div`
     width: 100%;
     height: 20%;
     font-size: 3rem;
     font-family: 'NovaRound';
     margin-bottom: 1rem;
 `;
-const StyledSignUpLoginContainer = styled.div`
+export const StyledSignUpLoginContainer = styled.div`
     width: 100%;
     height: 80%;
     font-size: 1.25rem;
@@ -94,25 +94,25 @@ const StyledSignUpLoginContainer = styled.div`
     font-weight: 600;
 `;
 
-const StyledSignUpLoginBox = styled.div`
+export const StyledSignUpLoginBox = styled.div`
     width: 100%;
     height: 30%;
     padding: 1rem 1rem;
 `;
 
-const StyledSignUpLoginLink = styled(Link)`
+export const StyledSignUpLoginLink = styled(Link)`
     color: var(--oboGreen);
     margin-left: 2rem;
     cursor: pointer;
 `;
 
-const StyledSignUpLogoBox = styled.div`
+export const StyledSignUpLogoBox = styled.div`
     width: 40%;
     height: auto;
     display: flex;
     justify-content: space-around;
 `;
-const StyledSignUpLogoImage = styled.img`
+export const StyledSignUpLogoImage = styled.img`
     width: 4rem;
     height: 4rem;
     border: 1px solid var(--gray-color);
@@ -120,45 +120,43 @@ const StyledSignUpLogoImage = styled.img`
     cursor: pointer;
 `;
 
-const StyledSignUpHorizontalRuleBox = styled.div`
+export const StyledSignUpHorizontalRuleBox = styled.div`
     width: 100%;
     height: auto;
-    margin-top: 2rem;
+    margin-top: 1rem;
     display: flex;
     justify-content: space-between;
     font-family: 'Heebo';
     font-weight: 300;
     color: var(--input-text-color);
 `;
-const StyledOrText = styled.div`
+export const StyledOrText = styled.div`
     width: 10%;
     display: flex;
     justify-content: center;
     align-items: center;
 `;
-const StyledSignUpHorizontalRule = styled.hr`
+export const StyledSignUpHorizontalRule = styled.hr`
     width: 45%;
     height: 1px;
     color: var(--gray-color);
 `;
 
-const StyledSignUpFormWrapper = styled.form`
+export const StyledSignUpFormWrapper = styled.form`
     width: 100%;
     height: 60%;
+    display: flex;
+    flex-direction: column;
     margin-top: 1rem;
 `;
-const StyledInputContainer = styled.div`
-    width: 100%;
-    height: var(--input-height);
+export const StyledInputContainer = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
     margin-bottom: 1rem;
 `;
 
-const StyledInputField = styled.input`
-    width: 100%;
-    height: var(--input-height);
+export const StyledInputField = styled.input`
     padding: 1rem;
     margin-bottom: 1rem;
     border: 1px solid var(--gray-color);
@@ -166,16 +164,15 @@ const StyledInputField = styled.input`
     color: var(--input-text-color);
     background-color: var(--input-bg-color);
 `;
-const StyledNameInputField = styled(StyledInputField)`
-    width: 49%;
-    height: 100%;
+export const StyledNameInputField = styled(StyledInputField)`
     margin-bottom: 0;
+    width: 49%;
 `;
-const StyledSignUpSubmitButton = styled.button`
+export const StyledSignUpSubmitButton = styled.button`
     width: 100%;
     height: var(--input-height);
-    margin-top: 1rem;
     font-weight: 400;
+    margin-bottom: 1rem;
     font-size: 1.5rem;
     color: #ffffff;
     background-color: var(--oboCoral);
