61b48f12d1b4fcb1f2adcd992600ca7b6ada7487
Gyuri092 <ggr0927@gmail.com>
2023-04-25 23:03:47 +0900

    setting: 라우팅 설정 완료

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import TestPage from '@pages/TestPage';
 import SignUp from '@pages/SignUp';
 import Callback from '@/pages/Callback';
 import Login from '@/pages/Login';
 import ReadMe from '@/pages/ReadMe';
+import MainPage from '@/pages/MainPage';
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<TestPage />} />
+                <Route path="/" element={<MainPage />} />
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/readme/:id" element={<ReadMe />} />
